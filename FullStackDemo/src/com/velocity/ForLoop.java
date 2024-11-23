a1c074bc68385e477c15d54e76422b6a67f837e1
gokarna-18 <mundhe.gokarna18@gmail.com>
2024-11-23 09:31:51 +0530

    While loop program

--- FullStackDemo/src/com/velocity/ForLoop.java
+++ FullStackDemo/src/com/velocity/ForLoop.java
@@ -1,11 +1,13 @@
 package com.velocity;
 
 public class ForLoop {
-	
+
 	public static void main(String[] args) {
 		
-		for(int i = 1; i<=5 ; i++) {
-			
+		System.out.println("This is for loop code");
+
+		for (int i = 1; i <= 5; i++) {
+
 			System.out.println(i);
 		}
 	}
---
+++ FullStackDemo/src/com/velocity/WhileLoop.java
@@ -0,0 +1,18 @@
+package com.velocity;
+
+public class WhileLoop {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("This is while loop code");
+		
+		int i = 1;
+		while(i <=5) {
+			
+			System.out.println(i);
+			
+			i++;
+		}
+	}
+
+}
